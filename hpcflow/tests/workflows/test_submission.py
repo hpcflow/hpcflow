515b171595e96d8d164023bc31c5dc545c17f893
Adam <adam.jp@live.co.uk>
2025-01-07 09:51:43 +0000

    test: improve many jobscripts tests

--- hpcflow/tests/workflows/test_submission.py
+++ hpcflow/tests/workflows/test_submission.py
@@ -9,11 +9,14 @@ def test_zarr_metadata_file_modification_times_many_jobscripts(null_config, tmp_
     at-submit-metadata chunk files, then the submission at-submit-metadata group
     attributes."""
 
+    num_js = 30
     t1 = hf.Task(
         schema=hf.task_schemas.test_t1_conditional_OS,
         inputs={"p1": 100},
         sequences=[
-            hf.ValueSequence(path="resources.any.resources_id", values=list(range(10)))
+            hf.ValueSequence(
+                path="resources.any.resources_id", values=list(range(num_js))
+            )
         ],
     )
     wk = hf.Workflow.from_template_data(
@@ -25,9 +28,9 @@ def test_zarr_metadata_file_modification_times_many_jobscripts(null_config, tmp_
     wk.submit(add_to_known=False, status=False, cancel=True)
 
     mtime_meta_group = Path(wk.path).joinpath(".zattrs").stat().st_mtime
-    mtime_jobscript0_chunk = (
+    mtime_mid_jobscript_chunk = (
         Path(wk._store.path)
-        .joinpath(wk._store._get_jobscripts_metadata_arr(0).path, "0")
+        .joinpath(wk._store._get_jobscripts_metadata_arr(0).path, str(int(num_js / 2)))
         .stat()
         .st_mtime
     )
@@ -37,4 +40,4 @@ def test_zarr_metadata_file_modification_times_many_jobscripts(null_config, tmp_
         .stat()
         .st_mtime
     )
-    assert mtime_meta_group < mtime_jobscript0_chunk < mtime_submission_group
+    assert mtime_meta_group < mtime_mid_jobscript_chunk < mtime_submission_group
