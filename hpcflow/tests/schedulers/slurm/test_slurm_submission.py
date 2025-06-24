d8f627e142b1cb3eea83d50f777355baa4eaf193
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2025-06-24 14:37:21 +0100

    Test of SLURM partition selection

--- hpcflow/tests/schedulers/slurm/test_slurm_submission.py
+++ hpcflow/tests/schedulers/slurm/test_slurm_submission.py
@@ -12,3 +12,13 @@ def test_workflow_1(tmp_path: Path, null_config):
     p2 = wk.tasks[0].elements[0].outputs.p2
     assert isinstance(p2, hf.ElementParameter)
     assert p2.value == "201"
+
+
+@pytest.mark.slurm
+def test_workflow_2(tmp_path: Path, null_config):
+    hf.config.add_scheduler("slurm")
+    wk = make_test_data_YAML_workflow("workflow_2_slurm.yaml", path=tmp_path)
+    wk.submit(wait=True, add_to_known=False)
+    bash_location = wk.tasks[0].elements[0].outputs.bash_location
+    assert isinstance(bash_location, hf.ElementParameter)
+    assert "bash" in bash_location.value
